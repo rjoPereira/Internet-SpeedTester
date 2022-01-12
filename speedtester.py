43ac484a2f5fadd355420b011d50de6b2b3315f0
ricardo <jp.ricardo01@gmail.com>
2022-01-12 15:00:55 +0000

    fixed some minor bugs and added a requirements file

--- speedtester.py
+++ speedtester.py
@@ -1,5 +1,5 @@
+import time
 from datetime import datetime
-from time import sleep
 
 import speedtest
 import pandas as pd
@@ -12,17 +12,18 @@ def get_internet_info(st):
     now = datetime.now()
     date = now.strftime("%Y-%m-%d")
     current_time = now.strftime("%H:%M:%S")
-    return {"download": round(st.download()/10**6, 3),
-            "upload": round(st.upload()/10**6, 3),
+    return {"download": round(st.download() / 10 ** 6, 3),
+            "upload": round(st.upload() / 10 ** 6, 3),
             "ping": st.results.ping,
             "date": date,
             "time": current_time,
             }
 
 
-def get_hourly_stats(df, index, internet_info):
-    download = df.get("download")
-    upload = df.get("upload")
+def get_hourly_stats(df, index):
+    last_records = df.tail(NUMBER_OF_TESTS_PER_HOUR)
+    download = last_records.get("download")
+    upload = last_records.get("upload")
 
     stats = {"average download(Hour)": download.mean(),
              "minimum download(Hour)": download.min(),
@@ -32,33 +33,34 @@ def get_hourly_stats(df, index, internet_info):
              "maximum upload(Hour)": upload.max()}
 
     for key in stats:
-        print(f"{key}: {stats[key]}")
+        print(f"{key}: {stats[key]} Mbps")
         df.loc[index, key] = stats[key]
 
 
 def run(st, df):
-    internet_info = get_internet_info(st)
     number_of_tests = 0
     while True:
-        print("*"*25 + f"\ndownload: {internet_info.get('download')} Mbps\nupload: {internet_info.get('upload')} Mbps"
-                       f"\nping: {internet_info.get('ping')} ms\ndate: {internet_info.get('date')}\n"
-                       f"time: {internet_info.get('time')}")
+        start = time.time()
+        internet_info = get_internet_info(st)
+        print("*" * 25 + f"\ndownload: {internet_info.get('download')} Mbps\nupload: {internet_info.get('upload')} Mbps"
+                         f"\nping: {internet_info.get('ping')} ms\ndate: {internet_info.get('date')}\n"
+                         f"time: {internet_info.get('time')}")
 
         df = df.append(internet_info, ignore_index=True)
         number_of_tests += 1
+        time_elapsed = time.time() - start
 
         if number_of_tests == NUMBER_OF_TESTS_PER_HOUR:
             index = df.last_valid_index()
-            get_hourly_stats(df, index, internet_info)
+            get_hourly_stats(df, index)
             number_of_tests = 0
-        sleep(TIMEOUT)
 
         df.to_csv("data.csv", index=False)
-        internet_info = get_internet_info(st)
+        time.sleep((TIMEOUT - time_elapsed) if time_elapsed < TIMEOUT else 0)
 
 
 if __name__ == "__main__":
     st = speedtest.Speedtest()
     st.get_best_server()
-    df = pd.DataFrame()
+    df = pd.read_csv("data.csv")
     run(st, df)
